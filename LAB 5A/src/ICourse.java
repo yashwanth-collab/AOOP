f01cc89cb27eeb0214d6840443380e0b879fd793
yashwanth-collab <yashtechm1m2@gmail.com>
2024-09-12 12:02:05 +0530

    LAB

---
+++ LAB 5A/src/ICourse.java
@@ -0,0 +1,48 @@
+import java.util.List;
+
+// Interface Segregation Principle (ISP)
+// Define specific interfaces for different functionalities
+public interface ICourse {
+    String getId();
+    String getName();
+    List<Student> getStudents();
+    void enrollStudent(Student student);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
